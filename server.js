37b5a64af9115d188da7d0d8f6d607ce6a05e95c
michaelbir <mikux22@gmail.com>
2023-11-12 08:31:55 +0200

    first commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const recipeRouter = require('./routes/recipes.routes');
 const db = require('./db');
 const ingredientRouter = require('./routes/ingredients.routes');
+const fullRecipesRouter = require('./routes/fullRecipes.routes');
 const app = express();
 
 // app.get('/recipes', async (req, res)=> {
@@ -19,6 +20,8 @@ const app = express();
 app.use('/ingredients', ingredientRouter);
 
 app.use('/recipes', recipeRouter);
+
+app.use('/fullRecipes', fullRecipesRouter);
 
 app.listen(process.env.PORT || 3003, () => {
     console.log('Server is listening port 3000')
